796c3317d5f5ba7417b9c2da4e06328866acc5b0
mangle <manglestealth@163.com>
2015-11-15 20:07:49 +0800

    前台模板

---
+++ public/assets/js/yzisme.js
@@ -0,0 +1,14 @@
+function dispatch() {
+    var q = document.getElementById("q");
+    if (q.value != "") {
+        var url = 'http://www.google.com/search?q=site:v2ex.com/t%20' + q.value;
+        if (navigator.userAgent.indexOf('iPad') > -1 || navigator.userAgent.indexOf('iPhone') > -1 || navigator.userAgent.indexOf('iPhone') > -1) {
+            location.href = url;
+        } else {
+            window.open(url, "_blank");
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
